03877507ede8cd51185d68b36d314c3782012d5b
Thiago Sueto <herzenschein@gmail.com>
2022-10-10 13:34:41 -0300

    Put initialized member in next line

--- timesettings.cpp
+++ timesettings.cpp
@@ -8,7 +8,8 @@
 K_PLUGIN_CLASS_WITH_JSON(TimeSettings, "kcm_time.json")
 
 TimeSettings::TimeSettings(QObject *parent, const KPluginMetaData &data, const QVariantList &args)
-    : KQuickAddons::ManagedConfigModule(parent, data, args), m_config(new Time::Config(this))
+    : KQuickAddons::ManagedConfigModule(parent, data, args)
+    , m_config(new Time::Config(this))
 {
     setButtons(Help | Apply | Default);
     qmlRegisterAnonymousType<TimeSettings::Config>("org.kde.timesettings.private", 1);
